70272d508d57b93c056738384cdf2bddd8e4d5ef
Kishan Agarwal <kishanagarwal028@gmail.com>
2025-06-28 03:28:25 +0530

    fix: Comment out csrfMiddleware usage in index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(csrfMiddleware)
+// app.use(csrfMiddleware)
 connectDB();
 connectRedis();
 connectvectorDB();
