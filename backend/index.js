6a14ebfb45a933be2d3a9f5bc63506b69a945694
Kishan Agarwal <kishanagarwal028@gmail.com>
2025-07-17 16:52:47 +0530

    refactor: Comment out helmet and combinedSanitizer middleware for future review

--- backend/index.js
+++ backend/index.js
@@ -36,8 +36,8 @@ app.use(
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
-app.use(helmet());
-app.use(combinedSanitizer);
+// app.use(helmet());
+// app.use(combinedSanitizer);
 app.use( rateLimit({
   windowMs: 60 * 1000, // 1 minute
   max: 100, // Limit each IP to 100 requests per windowMs
