99f29a8b10da2e7070670c40d29a1cb0c47a421f
Kishan Agarwal <kishanagarwal028@gmail.com>
2025-07-17 15:29:30 +0530

    fix: Correct usage of xssSanitizer middleware in index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(helmet());
-app.use(xssSanitizer())
+app.use(xssSanitizer)
 app.use(mongoSanitize());
 app.use(rateLimit({
   windowMs:60 * 1000, // 1 minute
